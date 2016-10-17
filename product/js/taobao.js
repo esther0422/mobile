c7a200b9e27b083e4cb8000598c4a701b5892d2a
esther0422 <541184980@qq.com>
2016-10-17 13:24:43 +0800

    add TaoBao

---
+++ product/js/taobao.js
@@ -0,0 +1,1 @@
+function setRem(){document.documentElement.style.fontSize=innerWidth/20+"px"}setRem(),onresize=setRem;var close=document.querySelector(".ad .n2b-taobao a"),ad=document.querySelector(".ad");close.onclick=function(){ad.outerHTML=""};
